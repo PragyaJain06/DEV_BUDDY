3000ee77d9204f0ed6643fbf7c6be5a62ee5fd55
PragyaJain06 <140810236+PragyaJain06@users.noreply.github.com>
2025-07-16 16:43:59 +0530

    Update constants.js

--- client/src/Utils/constants.js
+++ client/src/Utils/constants.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "http://localhost:8081";
 // export const BASE_URL = "/api";
-export const BASE_URL="https://dev-buddy-six.vercel.app/";
+export const BASE_URL = "https://dev-buddy-six.vercel.app";
 
