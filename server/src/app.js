38a3893b19a5efd6cccd0f79c2f171fb8efc34b4
PragyaJain06 <140810236+PragyaJain06@users.noreply.github.com>
2025-07-16 16:01:46 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://dev-buddy-six.vercel.app",
     credentials: true,
   })
 );
