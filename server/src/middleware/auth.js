314d10bbf9d283a169af27773317677cdf4756a9
PragyaJain06 <140810236+PragyaJain06@users.noreply.github.com>
2025-07-17 16:37:02 +0530

    Update auth.js

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -3,6 +3,8 @@ const UserModel = require("../model/user");
 const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
+        console.log("JWT_TOKEN from env:", process.env.JWT_TOKEN);
+
     let decodedData = jwt.verify(token, process.env.JWT_TOKEN);
     const user = await UserModel.findById(decodedData?._id);
     if (!user) {
