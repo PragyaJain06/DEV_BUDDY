52d27fa85c7707e26b44ddbfc2bcd652a185a143
PragyaJain06 <140810236+PragyaJain06@users.noreply.github.com>
2025-07-20 13:34:59 +0530

    Update cronJob.js

--- server/src/utils/cronJob.js
+++ server/src/utils/cronJob.js
@@ -1,31 +1,31 @@
-const cron = require("node-cron");
-const { subDays, endOfDay, startOfDay } = require("date-fns");
-const ConnectionRequest = require("../model/connectionRequest");
-const sendEmail = require("./sendEmail");
-cron.schedule("0 8 * * *", async () => {
-  try {
-    const yesterday = subDays(new Date(), 1);
-    const yesterdayEnd = endOfDay(yesterday);
-    const yesterDayStart = startOfDay(yesterday);
-    const pendingRequests = await ConnectionRequest.find({
-      status: "interested",
-      createdAt: {
-        $gte: yesterDayStart,
-        $lt: yesterdayEnd,
-      },
-    }).populate("fromUser toUser");
+// const cron = require("node-cron");
+// const { subDays, endOfDay, startOfDay } = require("date-fns");
+// const ConnectionRequest = require("../model/connectionRequest");
+// const sendEmail = require("./sendEmail");
+// cron.schedule("0 8 * * *", async () => {
+//   try {
+//     const yesterday = subDays(new Date(), 1);
+//     const yesterdayEnd = endOfDay(yesterday);
+//     const yesterDayStart = startOfDay(yesterday);
+//     const pendingRequests = await ConnectionRequest.find({
+//       status: "interested",
+//       createdAt: {
+//         $gte: yesterDayStart,
+//         $lt: yesterdayEnd,
+//       },
+//     }).populate("fromUser toUser");
 
-    const listOfEmails = [
-      ...new Set(pendingRequests?.map((item) => item?.toUser?.email)),
-    ];
-    for (let email of listOfEmails) {
-      await sendEmail(
-        email,
-        "Someone is interested in you",
-        `You have pending requests pending.`
-      );
-    }
-  } catch (e) {
-    console.log(e, "error");
-  }
-});
+//     const listOfEmails = [
+//       ...new Set(pendingRequests?.map((item) => item?.toUser?.email)),
+//     ];
+//     for (let email of listOfEmails) {
+//       await sendEmail(
+//         email,
+//         "Someone is interested in you",
+//         `You have pending requests pending.`
+//       );
+//     }
+//   } catch (e) {
+//     console.log(e, "error");
+//   }
+// });
